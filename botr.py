e243cd489932c64acea9f91ca98dc57d4f08f6a6
Zynro <33652950+Zynro@users.noreply.github.com>
2019-03-26 03:20:54 -0400

    Bounty List Replacement Fully replaced and revamped the bounty list, each shikigami is now a class instance of Shikigami, with it's elements as instance variables. Function to download and overwrite csv database implemented Checks to only allow bot owners and editors to download and update implemented Added traceback for cog loading. Minor bot cleaning Deleted test scripts and json to csv converter script
    TODO: Version checker to see if a update of the database is required.

--- botr.py
+++ botr.py
@@ -5,7 +5,7 @@ import requests
 import config
 import bot_token
 
-initial_extensions = ['cogs.admin', 'cogs.voicecmd', 'cogs.onmyoji',]
+initial_extensions = ['cogs.admin', 'cogs.voicecmd', 'cogs.onmyoji']
 
 def get_prefix(bot, message):
     prefixes = ['&']
@@ -25,27 +25,29 @@ async def on_ready():
         for extension in initial_extensions:
             try:
                 bot.load_extension(extension)
+                print(extension +' has been loaded!')
             except Exception as e:
                 print(f'Failed to load extension {extension}.', file=sys.stderr)
                 traceback.print_exc()
+    print('------')
     print(f'Bathbot is fully ready!')
     if not discord.opus.is_loaded():
             discord.opus.load_opus('libopus.so')
             print('Opus has been loaded!')
 
-@bot.command(name='b-load', hidden=True)
-async def cog_load_backup(self, ctx, *, cog: str):
+'''@bot.command(name='load',hidden=True)
+async def cog_load(bot, ctx, *, cog: str):
     """Command which Loads a Module.
     Remember to use dot path. e.g: cogs.owner"""
     try:
-        self.bot.load_extension('cogs.'+cog)
+        bot.load_extension('cogs.'+cog)
     except Exception as e:
         await ctx.send(f'**ERROR:** {type(e).__name__} - {e}')
     else:
         await ctx.send('**Success: **cogs.'+cog+' has been loaded!')
 
-@bot.command(name='b-unload', hidden=True)
-async def cog_unload_backup(self, ctx, *, cog: str):
+@bot.command(name='unload',hidden=True)
+async def cog_unload(ctx, *, cog: str):
     """Command which Unloads a Module.
     Remember to use dot path. e.g: cogs.owner"""
     if ctx.message.author.id != owner:
@@ -53,23 +55,22 @@ async def cog_unload_backup(self, ctx, *, cog: str):
         return
     else:
         try:
-            self.bot.unload_extension('cogs.'+cog)
+            bot.unload_extension('cogs.'+cog)
         except Exception as e:
             await ctx.send(f'**ERROR:** {type(e).__name__} - {e}')
         else:
             await ctx.send('**Success:** cogs.'+cog+' has been unloaded!')
 
-@bot.command(name='b-reload', hidden=True)
-async def cog_reload(self, ctx, *, cog: str):
+@bot.command(name='reload', hidden=True)
+async def cog_reload(ctx, *, cog: str):
     """Command which Reloads a Module.
     Remember to use dot path. e.g: cogs.owner"""
     try:
-        self.bot.unload_extension('cogs.'+cog)
-        self.bot.load_extension('cogs.'+cog)
+        bot.reload_extension('cogs.'+cog)
     except Exception as e:
         await ctx.send(f'**Error:** {type(e).__name__} - {e}')
     else:
-        await ctx.send('**Success:** cogs.'+cog+' has been reloaded!')
+        await ctx.send('**Success:** cogs.'+cog+' has been reloaded!')'''
 
 
 bot.run(bot_token.TOKEN)
--- cogs/onmyoji.py
+++ cogs/onmyoji.py
@@ -1,18 +1,92 @@
+from __future__ import print_function
 import discord
 import json
 from discord.ext import commands
+import os.path
+from googleapiclient.discovery import build
+from googleapiclient.http import *
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2 import service_account
+import io
+import csv
+import config
+import re
 
+permission = 'You do not have permission to use this command.'
+owner_list = config.owner_list
+editor_list = config.editor_list
 
-with open('lists/bounty.json') as file:
-    bounty_json = json.loads(file.read())
+recommend = re.compile(r'recommend',re.IGNORECASE)
+
+#=============Drive API Stuff===============
+SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+
+SERVICE_ACCOUNT_FILE = 'bbt_credentials.json'
+
+credentials = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+drive_service = build('drive', 'v3', credentials = credentials)
+#=============Drive API Stuff===============
+
+#Deactivated backup code
+#with open('lists/bounty.json') as file:
+#    bounty_json = json.loads(file.read())
 
 with open('lists/stats.json') as file:
     stats_json = json.loads(file.read())
+
+def bold(input):
+    return "**"+input+"**"
+
+class Shikigami:
+    def __init__(self, input_name, bounty_db):
+        self.name = input_name
+        for row in bounty_db:
+            shiki_name, alias, hints, locations = row[0], row[1], row[2], row[3]
+            if input_name in shiki_name:
+                self.alias = alias
+                self.hints = hints
+                self.locations = locations.split('\n')
+            elif self.name.lower() in alias.lower():
+                self.name = shiki_name
+                self.alias = alias
+                self.hints = hints
+                self.locations = locations
 
 
 class Onmyoji(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.bounty_list = []
+        with open('lists/shiki_bounty.csv', newline='') as bounties:
+            bounty_reader = csv.reader(bounties)
+            next(bounty_reader)
+            for row in bounty_reader:
+                self.bounty_list.append(row)
+        self.shikigami_class = {row[0].lower(): Shikigami(row[0], self.bounty_list) for row in self.bounty_list}
+        self.shikigami_list = [row[0].lower() for row in self.bounty_list]
+        self.bounty_list = []
+
+    async def has_permission(ctx):
+        if ctx.author.id in owner_list or ctx.author.id in editor_list:
+            return True
+        else:
+            return False
+
+    def shiki_found(self, shiki):
+        shiki_name = self.shikigami_class[shiki].name
+        return "I found the following Shikigami: **"+shiki_name+"**\nHere are their locations:\n--------------------"
+
+    def location_finder(self, shiki):
+        locations_base = [location for location in self.shikigami_class[shiki].locations]
+        for location in locations_base:
+            match_recommend = recommend.search(location)
+            if match_recommend:
+                locations_base[locations_base.index(location)] = bold(location)
+        locations_finished = '\n'.join(locations_base)
+        return locations_finished
 
     @commands.command()
     async def shikistats(self, ctx, *search):
@@ -35,20 +109,46 @@ class Onmyoji(commands.Cog):
             return
         await ctx.send(f'Couldn\'t find stats for {name.title()}')
 
-    @commands.command()    
-    async def bounty(self, ctx, *search):
-        search = ' '.join([term.lower() for term in search])
-        for item in bounty_json:
-            if (
-                item['name'].lower() == search or
-                search in [hint.lower() for hint in item['hints']]
-                ):
-                locations = '\n '.join([location for location in item["locations"]])
-                await ctx.send(f'I found a result for: {item["name"]}\n You should go to:\n {locations}')
-                return
-        print(ctx)
-        await ctx.send('I, for all of my bath powers, could not find that bounty.')
+    @commands.command(name='shikigami_update', aliases=['update', 'download'])
+    @commands.check(has_permission)
+    async def download_shikigami_update(self, ctx):
+        file_id = config.bounty_file_id
+        request = drive_service.files().export_media(fileId=file_id, mimeType='text/csv')
+        buffer_file = io.BytesIO()
+        downloader = MediaIoBaseDownload(buffer_file, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            print ("Download %d%%." % int(status.progress() * 100))
+        with open ('lists/shiki_bounty.csv', 'wb') as f:
+            f.write(buffer_file.getvalue())
+        await ctx.send("The Shikigami Bounty list has been successfully updated!")
 
+    @download_shikigami_update.error
+    async def download_shikigami_update_error(self, ctx, error):
+        if isinstance(error, commands.CheckFailure):
+            await ctx.send("You do not have permission to update the database file.\nPlease tag an @Officer to have them update it.")
+
+    @commands.command()
+    async def bounty(self, ctx, *search):
+        if not search:
+            await ctx.send('Search term cannot be blank, try again.')
+            return
+        search = ' '.join([term.lower() for term in search])
+        for shiki in self.shikigami_class.keys():
+            if search in shiki:
+                await ctx.send(self.shiki_found(shiki))
+                await ctx.send(self.location_finder(shiki))
+                return
+            if search in self.shikigami_class[shiki].hints.lower():
+                await ctx.send(self.shiki_found(shiki))
+                await ctx.send(self.location_finder(shiki))
+                return
+            if search in self.shikigami_class[shiki].alias.lower():
+                await ctx.send(self.shiki_found(shiki))
+                await ctx.send(self.location_finder(shiki))
+                return
+        await ctx.send("For all my bath powers, I could not find your term, or something went wrong.")
         
 
 def setup(bot):
