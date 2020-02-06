a375bc61587ea2e56253dec1e78bc5df7bd02023
Zynro <33652950+Zynro@users.noreply.github.com>
2020-02-06 16:04:43 -0500

    [module] begin writing message_lib model into module add methods to manage message id's

--- models/module.py
+++ models/module.py
@@ -4,3 +4,30 @@ class Module:
         self.path = path
         self.cog_list = cog_list
         self.access = guild_access
+        self.msg_lib = {}
+
+    def add_msg(self, message):
+        try:
+            msg = int(message.id)
+        except AttributeError:
+            msg = int(message)
+        self.msg_lib[msg] = {"id": msg}
+
+    def del_msg(self, message):
+        try:
+            msg = int(message.id)
+        except AttributeError:
+            msg = int(message)
+        del self.msg_lib[msg]
+
+    async def fetch_msg(messageable, msg_id):
+        return await messageable.fetch_message(int(msg_id))
+
+    def set_vals(self, msg_id, vals):
+        for k in vals.keys():
+            try:
+                key = k.lower()
+                val = vals[k].lower()
+            except Exception:
+                val = vals[k]
+            setattr(self, key, val)
