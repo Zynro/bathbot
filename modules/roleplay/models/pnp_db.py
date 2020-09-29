32292d4ccf5acd134aac8a3dbd1338fc9fb71bc5
Zynro <33652950+Zynro@users.noreply.github.com>
2020-09-29 09:44:24 -0400

    [roleplay] added basic functionality to a ORM mapper for current guild pnp values

--- modules/roleplay/models/pnp_db.py
+++ modules/roleplay/models/pnp_db.py
@@ -1,35 +1,71 @@
 import os
 import json
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import orm
+
+supported_systems = ["mutant"]
 
 path_to_pnp_guilds = "modules/"
+Base = declarative_base()
+Session = orm.sessionmaker()
+
+engine = create_engine("sqlite:///pnp_guilds.db", echo=True)
 
 
-def init_pnp_guilds():
-    engine = create_engine("sqlite:///pnp_guilds.db", echo=True)
-    meta = MetaData()
-
-    guilds = Table(
-        "guilds",
-        meta,
-        Column("guild", Integer, primary_key=True),
-        Column("current", String),
-    )
-
-    systems = Table(
-        "systems",
-
-        )
-    meta.create_all(engine)
+def open(Session):
+    Session.configure(bind=engine)
+    session = Session()
+    if not os.path.exists(f"modules/roleplay/pnp_db.db"):
+        Base.metadata.create_all(engine)
+    return session
 
 
-class PnP_DB:
-    def __init__(self):
-        if not os.path.exists(f"modules/roleplay/pnp_db.db"):
-            init_pnp_guilds()
+class Guilds(Base):
+    __tablename__ = "guilds"
+    guild = Column(Integer, primary_key=True)
+    system = Column(String)
 
-    def set_system(self, system)
+    """def __init__(self, **kw):
+        for k in kw.keys():
+            k = k.lower()
+            setattr(self, k, kw[k])"""
+
+    def __init__(self, guild, system):
+        self.guild = guild
+        self.system = system
 
 
+class GUILD_PNP_DB(Guilds):
+    def __init__(self, Session):
+        self.Session = Session
 
-init_pnp_guilds()
+    def get_guild(self, guild_id, session=None):
+        if not session:
+            session = open(self.Session)
+            guild = session.query(Guilds).get(guild_id)
+            session.close()
+        else:
+            guild = session.query(Guilds).get(guild_id)
+        return guild
+
+    def set_system(self, guild_id, system):
+        session = open(self.Session)
+        if system.lower() in supported_systems:
+            if not session.query(Guilds).filter_by(guild=guild_id).scalar():
+                print("DOES NOT EXIST")
+                row = super.__init__(guild_id, system)
+                session.add(row)
+                session.commit()
+            else:
+                print("EXISTS")
+                guild = self.get_guild(guild_id, session)
+                guild.system = system
+                session.commit()
+        return session.close()
+
+
+threv = 616758392525946925
+db = GUILD_PNP_DB(Session)
+db.set_system(threv, "mutant")
+print(db.get_guild(threv).system)
